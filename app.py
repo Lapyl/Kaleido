8a9f1340d6d166eaee186ac5eb03153d4881b4b3
Lalit A Patel <Lapyl@users.noreply.github.com>
2023-07-27 17:51:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ import streamlit as st
 Python code:
 """
 
-with st.echo(code_location='below'):
+with st.echo(code_location='right'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
