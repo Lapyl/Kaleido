140d49fa7b46bd02cfc8f3f73bb6ae50decc5425
Lalit A Patel <Lapyl@users.noreply.github.com>
2023-07-27 18:29:02 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,11 @@ import streamlit as st
 
 """
 # Kaleido
-### Streamlit app Kaleido
-### GitHub repository Lapyl/Kaleido
-
+Streamlit app Kaleido
+GitHub repository Lapyl/Kaleido
 - [GitHub Repository](https://github.com/Lapyl/Kaleido)
 - [Streamlit Dcumentation](https://docs.streamlit.io)
 - [Streamlit Forums](https://discuss.streamlit.io)
-
 Python code:
 """
 
