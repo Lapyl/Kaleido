acc5619ba1e64890d3b3a2b9caa7a4c5ba6e3413
Lalit A Patel <Lapyl@users.noreply.github.com>
2023-07-27 17:23:20 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,18 +5,17 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+# Kaleido
+## Streamlit app Kaleido <=> GitHub repository Lapyl/Kaleido
 
-Edit `/app.py` to customize this app to your heart's desire :heart:
+- [GitHub Repository](https://github.com/Lapyl/Kaleido)
+- [Streamlit Dcumentation](https://docs.streamlit.io)
+- [Streamlit Forums](https://discuss.streamlit.io)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+Python code:
 """
 
-
-with st.echo(code_location='below'):
+with st.echo(code_location='above'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
